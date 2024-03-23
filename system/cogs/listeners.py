bac6de4fdbf2a87328fbac9326550c198558ebdf
Crone720 <143301210+Crone720@users.noreply.github.com>
2024-03-23 16:35:25 +0300

    Обновил listeners.py
    Крокус 🥀

--- system/cogs/listeners.py
+++ system/cogs/listeners.py
@@ -25,6 +25,33 @@ class Listeners(commands.Cog):
 
             await db.commit()
 
+from disnake.ext import commands
+from disnake.ext.commands.errors import MissingPermissions, CommandOnCooldown
+import aiosqlite, disnake, datetime
+
+class Listeners(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        async with aiosqlite.connect("system/ticket.db") as db:
+            cursor = await db.cursor()
+            await cursor.execute("""
+            CREATE TABLE IF NOT EXISTS Channels (
+                userid INTEGER,
+                channelid INTEGER
+            )
+            """)
+            await cursor.execute("""
+            CREATE TABLE IF NOT EXISTS bans (
+                userid INTEGER PRIMARY KEY,
+                reason TEXT
+            )
+            """)
+
+            await db.commit()
+
     @commands.Cog.listener()
     async def on_slash_error(self, inter, error):
         if isinstance(error, MissingPermissions):
@@ -33,12 +60,17 @@ class Listeners(commands.Cog):
         elif isinstance(error, CommandOnCooldown):
             time = disnake.utils.format_dt(datetime.datetime.now() + datetime.timedelta(seconds=error.retry_after), 'R')
             embedcooldown = disnake.Embed(
-                title='Ошибка',
+                title='Обращение',
                 description=f'{inter.author.mention}, Вы сможете использовать эту команду '
                             f'через {time}',
                 color=0x2F3136
             )
             await inter.send(embed=embedcooldown, ephemeral=True)
+        elif isinstance(error, commands.MissingRole):
+            embedmissingrole = disnake.Embed(title="Обращение", description="Вы не имеете необходимой роли для использования команды")
+            await inter.send(embed=embedmissingrole, ephemeral=True)
+def setup(bot):
+    bot.add_cog(Listeners(bot))
             
 def setup(bot):
     bot.add_cog(Listeners(bot))
